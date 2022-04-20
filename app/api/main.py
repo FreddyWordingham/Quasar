63f9c2c1a2eafc4a1e3379ee2b53e7bae5a3cbf8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-04-20 23:24:42 +0100

    Added FastAPI application

--- app/api/main.py
+++ app/api/main.py
@@ -1,22 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-
-from . import settings
-from .component import component_route
-from .hmi import hmi_route, Session
-from .runner import runner_route, Session
-
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-app.include_router(component_route, prefix="/component")
-app.include_router(hmi_route, prefix="/hmi")
-app.include_router(runner_route, prefix="/runner")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def splashpage(request: Request):
-    return settings.templates.TemplateResponse(
-        "index.html", {"request": request, "sessions": Session.data.keys()}
-    )
+    return "<h1>Hello world!</h1>"
