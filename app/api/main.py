fbd8122eccb9aa794ccd6ecaa9cee2c0e5cf3841
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-04-20 23:56:58 +0100

    Added application files

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+from . import settings
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def splashpage(request: Request):
-    return "<h1>Hello world!</h1>"
+    return settings.TEMPLATES.TemplateResponse("index.html", {"request": request})
---
+++ app/api/settings.py
@@ -0,0 +1,10 @@
+from fastapi.templating import Jinja2Templates
+import os
+
+APP_DIR = "app"
+TEMPLATES_DIR = os.path.join(APP_DIR, "templates")
+PLUGIN_DIR = os.path.join(TEMPLATES_DIR, "plugins")
+STATIC_DIR = os.path.join(APP_DIR, "static")
+RESOURCES_DIR = os.path.join(STATIC_DIR, "resources")
+
+TEMPLATES = Jinja2Templates(directory=TEMPLATES_DIR)
