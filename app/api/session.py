7032ff4c2a6addb2de086a3e99d011f328f97e6f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-04-21 14:43:26 +0100

    Improved get_plugin_list utility method

--- app/api/session.py
+++ app/api/session.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 import re
 
-from . import settings
+from . import settings, utility
 
 
 session_route = APIRouter()
@@ -21,18 +21,16 @@ async def load(request: Request, session_id: str):
     if session_id not in Session.data.keys():
         raise ValueError(f"Session: '{session_id}' does not exist.")
 
+    active_plugins = [
+        ["default", item_name] for item_name in utility.get_plugin_list("default")
+    ]
+
     return settings.TEMPLATES.TemplateResponse(
         "session.html",
         {
             "request": request,
             "session_id": session_id,
-            "active_plugins": [
-                ["default", "draggable"],
-                ["default", "minimise"],
-                ["default", "resize"],
-                ["default", "session"],
-                ["private", "test"],
-            ],
+            "active_plugins": active_plugins,
         },
     )
 
@@ -43,7 +41,7 @@ async def new(session_id: str):
     Create a new session.
     """
 
-    session_id = re.sub("[^a-zA-Z\d:-]", "", session_id)
+    session_id = re.sub("[^a-zA-Z\d:-_]", "", session_id)
 
     if session_id in Session.data.keys():
         raise ValueError(f"Session: '{session_id}' already exists.")
