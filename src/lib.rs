bcb153da62c7e422c8677a775a89026ab5ec7fd3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-04-21 16:08:03 +0100

    Added main binary

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn hello_world() {
-        let greeting = "Hello world!";
-        assert_eq!(greeting, "Hello world!");
-    }
-}
+//! Library core.
+
+#![warn(
+    clippy::all,
+    clippy::cargo,
+    clippy::missing_docs_in_private_items,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::restriction
+)]
+#![allow(
+    clippy::as_conversions,
+    clippy::cast_possible_truncation,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::blanket_clippy_restriction_lints,
+    clippy::default_numeric_fallback,
+    clippy::else_if_without_else,
+    clippy::exhaustive_structs,
+    clippy::expect_used,
+    clippy::float_arithmetic,
+    clippy::implicit_return,
+    clippy::indexing_slicing,
+    clippy::integer_arithmetic,
+    clippy::missing_panics_doc,
+    clippy::module_name_repetitions,
+    clippy::panic
+)]
