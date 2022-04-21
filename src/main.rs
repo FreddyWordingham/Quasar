ce8ae8c474f4ebec4a6308bc00757be6c3b8b28a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-04-21 16:28:45 +0100

    Added server loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use std::path::PathBuf;
+
+use quasar::args;
+
 /// Handles a single computation session.
+/// Watch an input file for newly added lines,
+/// calling a processing function with any new commands.
+/// After each check, sleep for the given interval [sec].
 fn main() {
-    println!("Hello, world!");
+    args!(_bin_path: PathBuf, session_id: String, sleep_time: f32);
+
+    let interval = (sleep_time * 1000.0) as u64;
+
+    let mut loop_counter = 0;
+    loop {
+        println!("[{}] {:4} > Hello, world!", session_id, loop_counter);
+
+        std::thread::sleep(std::time::Duration::from_millis(interval));
+        loop_counter += 1;
+    }
 }
