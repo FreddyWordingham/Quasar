2624f1d64f226377c4828d48af9f0bf34e6a0ec8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-04-22 19:08:50 +0100

    Added input_dir config parameter

--- src/bin/render.rs
+++ src/bin/render.rs
@@ -11,6 +11,8 @@ use quasar::{
 /// Configuration object.
 #[derive(Deserialize)]
 pub struct Config {
+    /// Path to the top level resource directory.
+    pub input_dir: PathBuf,
     /// Path to the output directory.
     pub output_dir: PathBuf,
     /// Output image resolution.
