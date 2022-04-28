ff5d9dbe4cf8c93f3590722d829e22923f9383a2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-04-28 14:26:18 +0100

    Simplified ParametersBuilder

--- src/render/parameters.rs
+++ src/render/parameters.rs
@@ -4,16 +4,13 @@ use std::path::PathBuf;
 
 /// Runtime data.
 pub struct Parameters {
-    /// Path to the top level resource directory.
-    pub input_dir: PathBuf,
     /// Path to the output directory.
     pub output_dir: PathBuf,
 }
 
 impl Parameters {
-    pub fn new(input_dir: PathBuf, output_dir: PathBuf) -> Self {
+    pub fn new(output_dir: PathBuf) -> Self {
         Self {
-            input_dir: input_dir,
             output_dir: output_dir,
         }
     }
--- src/render/parameters_builder.rs
+++ src/render/parameters_builder.rs
@@ -7,13 +7,13 @@ use crate::render::Parameters;
 /// Input configuration.
 pub struct ParametersBuilder {
     /// Path to the top level resource directory.
-    pub input_dir: PathBuf,
+    _input_dir: PathBuf,
     /// Path to the output directory.
     pub output_dir: PathBuf,
 }
 
 impl ParametersBuilder {
     pub fn build(self) -> Parameters {
-        Parameters::new(self.input_dir, self.output_dir)
+        Parameters::new(self.output_dir)
     }
 }
