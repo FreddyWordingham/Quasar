0861aafbcc43a65309a173867826ac7e46cc9e3b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-05-09 19:23:30 +0100

    Removed duplicate code

--- app/api/session.py
+++ app/api/session.py
@@ -3,11 +3,12 @@ from pydantic import BaseModel
 from subprocess import Popen
 import glob
 import json
-import math
 import os
 import re
 import shutil
 import signal
+
+from script.stitch import stitch
 
 from . import component, settings
 
@@ -205,33 +206,3 @@ async def stitch_tiles(session_id: str):
     """
 
     stitch(os.path.join(settings.SESSIONS_DIR, session_id, "tiles"), "colour")
-
-
-def stitch(input_dir, kind):
-    """
-    Stich together the individual render tiles at a given patturn.
-    """
-
-    pattern = os.path.join(input_dir, kind)
-
-    tiles = glob.glob(f"{pattern}_*")
-    width = 0
-    height = 0
-    for name in tiles:
-        parts = name.split("_")
-        xi = int(parts[-2])
-        yi = int(parts[-1].split(".")[0])
-        if xi > width:
-            width = xi
-        if yi > height:
-            height = yi
-
-    print_width = int(math.log10(max(width, height))) + 1
-    for n in range(height + 1):
-        slice_patturn = f"{pattern}_{n:0{print_width}}_*"
-        os.system(
-            f"convert -append {slice_patturn} {pattern}-slice-{n:0{print_width}}.png"
-        )
-
-    os.system(f"convert +append {pattern}-slice-* {os.path.join(input_dir, kind)}.png")
-    os.system(f"open {pattern}.png")
